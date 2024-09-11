3fe18f52721b97f6446a0a3718ed5d621c421f3f
shivam33589 <126815823+shivam33589@users.noreply.github.com>
2024-09-12 03:09:21 +0530

    Add files via upload

---
+++ Rules.java
@@ -0,0 +1,74 @@
+//package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to Java Quiz");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>"+ 
+                "1. In this round each one will be asked 10 questions one after another." + "<br><br>" +
+                "2. On the immediate completion of a question, each person gets 15 seconds to answer it." + "<br><br>" +
+                "3. If a person is not able to answer a question, they can pass it for the next question." + "<br><br>" +
+                "4. It is mandatory to attend all the questions." + "<br><br>" +
+                "5. Help key will remove 2 incorrect option." + "<br><br>" +
+                "6. 1 points is awarded for the correct answer. No negative points for the wrong answer" + "<br><br>" +
+                "7. Once a question is passed the team cannot give an answer later." + "<br><br>" +
+                "8. Good Luck" + "<br><br>" +
+            "<html>"
+        );
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+    
+    public static void main (String[] args) {
+        new Rules("User");
+    }
+}
+    
+
